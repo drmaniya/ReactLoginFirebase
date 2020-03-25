79358e20c8086a7f9793939b454526e6e167ae1b
drmaniya <49053624+drmaniya@users.noreply.github.com>
2020-03-25 10:28:25 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 require('firebase/firestore');
   var firebaseConfig = {
-    apiKey: "AIzaSyBlSWaJgaBJ138w4Tf8bycxW_1nq3bOshY",
-    authDomain: "react-demo-2f2ee.firebaseapp.com",
-    databaseURL: "https://react-demo-2f2ee.firebaseio.com",
-    projectId: "react-demo-2f2ee",
-    storageBucket: "react-demo-2f2ee.appspot.com",
-    messagingSenderId: "649696858319",
-    appId: "1:649696858319:web:11374fc0695b5c35"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   // Initialize Firebase
   const fb = firebase.initializeApp(firebaseConfig);
